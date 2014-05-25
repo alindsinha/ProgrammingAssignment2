df294fe22fa5d2553b3ad8dbd29d69505f92ac48
Alind <alindsinha@gmail.com>
2014-05-26 03:01:31 +0530

    A function to demonstrate lexical scoping in R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions are used to demonstrate lexical scoping in R
+## 
 
-## Write a short comment describing this function
+## The fits function takes in a matrix as a input and returns 
+## a list of functions where in the functions are used to set and get the 
+## value by the use of "<<-" operator which edits the values in the parent function
 
 makeCacheMatrix <- function(x = matrix()) {
+m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function takes a list of functions as an input with the other 
+## default values and tries to re calculate the inverse of the matrix-if it
+## is unable to retrieve it from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		 m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
